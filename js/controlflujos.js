a76865dd6539e8e6e0e4ba1005f1ce845beda701
Ale Piedrabuena <alejopiedrabuena@gmail.com>
2022-05-24 00:55:34 -0300

    first class' work

---
+++ js/controlflujos.js
@@ -0,0 +1,9 @@
+let valor1;
+let valor2;
+
+valor1=parseInt(prompt("Ingresá un número"));
+valor2=parseInt(prompt("Ingresa otro número para sumarlo"));
+
+result = valor1 + valor2;
+
+alert("La sumá de los dos números es: " + result);
